b971e36b271269b6ba3a8098efc678c25d35a350
Lucas Rezende <66080424+Lucas-Rezende@users.noreply.github.com>
2025-02-22 19:36:16 -0300

    fix astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://Lucas-Rezende.github.io',
+  site: 'https://Lucas-Rezende.github.io/MOSAIC/',
+  base: '/MOSAIC/',
 })
