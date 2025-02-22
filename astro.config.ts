42999f8147d80fce14249b8fbefa19272e55c126
Lucas Rezende <66080424+Lucas-Rezende@users.noreply.github.com>
2025-02-22 20:09:00 -0300

    add base

--- astro.config.ts
+++ astro.config.ts
@@ -22,7 +22,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  site: 'https://Lucas-Rezende.github.io',
+  site: 'https://lucas-rezende.github.io',
+  base: '/MOSAIC'
   output: 'static',
 
   integrations: [
