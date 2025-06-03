e07d28c3934e48c2c853751e542323d3a8e2084e
Lucas-Rezende <lucasmomede@yahoo.com.br>
2025-06-03 14:48:05 -0300

    Update astro.config.ts e build atualizado

--- astro.config.ts
+++ astro.config.ts
@@ -22,8 +22,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  site: 'https://mosaic-ufmg.github.io/mosaic-ufmg/',
-  base: '/mosaic-ufmg/',
+  site: 'https://mosaic-ufmg.github.io/',
   output: 'static',
 
   integrations: [
